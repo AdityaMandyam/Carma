54b763afab023943055af521a3d46982c855a515
AdityaMandyam <aditya.mandyam@gmail.com>
2017-01-25 18:21:12 -0800

    TODO: Persistent Storage, History Page, OCR

--- Carma/Dashboard.swift
+++ Carma/Dashboard.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Charts
 
 class FillUp { // fillup class to store data for each visit to gas station
     private var money: Double; // amount spent
@@ -42,9 +43,9 @@ class FillUp { // fillup class to store data for each visit to gas station
 
 var FillUps = [FillUp](); // fillups array to hold data
 
-class Dashboard: UIViewController {
+class Dashboard: UIViewController, ChartViewDelegate {
 
-    @IBOutlet var lineChart: LineChartView!
+    @IBOutlet weak var chart: BarChartView!
     @IBOutlet weak var mileageLabel: UILabel!
     @IBOutlet weak var spentLabel: UILabel!
     
@@ -94,6 +95,12 @@ class Dashboard: UIViewController {
         }
         // Do any additional setup after loading the view, typically from a nib.
         
+        self.chart.delegate = self;
+        chart.noDataText = "Not enough data yet!";
+
+        if (FillUps.count >= 2) {
+            setChart(data_points: FillUps);
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -101,6 +108,107 @@ class Dashboard: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    /*
+     
+     Setup for line chart
+     
+     */
+    func setChart(data_points: [FillUp]) {
+        var gallons = [Double]();
+        var index = data_points.count - 1;
+        while(index >= 0) {
+            gallons.append(data_points[index].getGallons());
+            if gallons.count >= 10 {
+                break;
+            }
+            index -= 1;
+        }
+        gallons = gallons.reversed();
+        /*
+        var yvals: [ChartDataEntry] = [ChartDataEntry]();
+        for index in 0 ..< dates.count {
+            yvals.append(ChartDataEntry(x: gallons[index], y: Double(index)));
+        }
+        let set1: LineChartDataSet = LineChartDataSet(values: yvals, label: "gallons");
+        
+        set1.axisDependency = .left;
+        set1.setColor(UIColor.red.withAlphaComponent(0.5));
+        set1.setCircleColor(UIColor.red);
+        set1.lineWidth = 2.0;
+        set1.circleRadius = 6.0;
+        set1.fillAlpha = 65 / 255.0;
+        set1.fillColor = UIColor.red;
+        set1.highlightColor = UIColor.white;
+        set1.drawCircleHoleEnabled = true;
+        
+        let chartData = LineChartData(dataSet: set1);
+        chart.data = chartData;
+        */
+        
+        
+        
+        /*
+         func setChart(dataPoints: [String], values: [Double]){
+            historyGraph!.noDataText = "Not enough data yet";
+            var dataEntries: [BarChartDataEntry] = []
+         
+            for i in 0..<dataPoints.count {
+                let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+                dataEntries.append(dataEntry)
+            }
+         
+            let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "GallonsPurchased")
+            let chartData = BarChartData(xVals: dates, dataSet: chartDataSet)
+            historyGraph!.data = chartData
+         }
+ 
+        
+        var data_entries: [BarChartDataEntry] = [];
+        for i in (0...gallons.count - 1).reversed() {
+            let data_entry = BarChartDataEntry(x: Double(i), y: gallons[i]);
+            data_entries.append(data_entry);
+        }
+        
+        let chart_data_set = BarChartDataSet(values: data_entries, label: "Gallons Purchased");
+        chart_data_set.setColor(UIColor.red.withAlphaComponent(1.00));
+        chart_data_set.barBorderColor = NSUIColor.black;
+        let chart_data = BarChartData(dataSet: chart_data_set);
+        chart.animate(xAxisDuration: 1000, yAxisDuration: 1000);
+        chart.chartXMin = 0
+        chart.data = chart_data
+        */
+        
+        var data_entries = [BarChartDataEntry]();
+        for i in 0..<gallons.count {
+            let data_entry = BarChartDataEntry(x: Double(i), yValues: [gallons[i]]);
+            data_entries.append(data_entry);
+        }
+        let chart_data_set = BarChartDataSet(values: data_entries.reversed(), label: "Gallons purchased");
+        let chart_data = BarChartData(dataSet: chart_data_set);
+        chart_data_set.barBorderColor = UIColor.black;
+        chart_data_set.barBorderWidth = 2.0;
+        chart_data_set.setColor(UIColor.red);
+        let lyaxis = chart.getAxis(.left);
+        lyaxis.axisMinimum = 0
+        lyaxis.enabled = false;
+        let ryaxis = chart.getAxis(.right);
+        ryaxis.enabled = false;
+        let xaxis = chart.xAxis;
+        xaxis.enabled = false;
+        xaxis.axisMinimum = -1;
+        xaxis.granularity = 1;
+        chart.drawGridBackgroundEnabled = false;
+        chart.chartDescription?.text = "";
+        chart.backgroundColor = NSUIColor.lightGray;
+        chart.borderColor = NSUIColor.black;
+        chart.pinchZoomEnabled = false;
+        chart.scaleXEnabled = false;
+        chart.scaleXEnabled = false;
+        chart.doubleTapToZoomEnabled = false;
+        chart.xAxis.drawGridLinesEnabled = false;
+        chart.animate(yAxisDuration: 1)
+        chart.data = chart_data;
+    }
 }
 
